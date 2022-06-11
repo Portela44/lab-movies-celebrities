97071394447fc2b9241a295caeeb9567b7e42d68
Guillem <guillem.mart7@gmail.com>
2022-06-11 18:13:48 +0200

    iteration 1 done, all files created and linked

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* GET movies page */
+router.get("/movies", (req, res, next) => {
+  res.render("movies/movies");
+})
+
+/* GET celebrities page */
+router.get("/celebrities", (req, res, next) => {
+  res.render("celebrities/celebrities");
+})
+
 module.exports = router;
