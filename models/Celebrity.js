e01c6fa20baca2cace6c58c428df6b9562d1c516
Guillem <guillem.mart7@gmail.com>
2022-06-11 18:26:46 +0200

    iteration 2 done, celebrity model created

--- models/Celebrity.js
+++ models/Celebrity.js
@@ -1,1 +1,22 @@
 //  Add your code here
+const {Schema, model} = require("mongoose");
+
+const celebritySchema = new Schema ({
+    name: {
+        type:String,
+        unique:true,
+        required:true
+    },
+    occupation: {
+        type:String,
+        required:true
+    },
+    catchPhrase: {
+        type:String,
+        required:true,
+    }
+})
+
+const Celebrity = model("celebrity", celebritySchema);
+
+module.exports = Celebrity;
